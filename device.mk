25b61cf591150fd23377a6520fd9d3aa6ef0826c
Abdallah Ibrahim <iabdollah40@gmail.com>
2025-07-20 05:39:42 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/realme/RMX1971
+
 #========================================
 # Platform & Inheritance
 #========================================
